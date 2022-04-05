c02e11c705f3c38ab17540fd55b988379321b9a0
nirch <nirh2@yahoo.com>
2022-04-06 00:50:06 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import noteRoutes from "./routes/noteRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import logRoute from "./middlewares/logRoute.js";
 
 const app = new express();
 
+app.use(cors());
 app.use(express.json());
 app.use(logRoute);
 
