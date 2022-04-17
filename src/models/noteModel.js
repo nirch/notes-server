6885dd20c53c73900773ba3393434ad7604a493f
nirch <nirh2@yahoo.com>
2022-04-17 11:43:16 +0300

    using better knex api (e.g. first)

--- src/models/noteModel.js
+++ src/models/noteModel.js
@@ -3,14 +3,14 @@ import db from "../data/db.js";
 
 async function getNotes(userId) {
   const notes = userId
-    ? await db.from("notes").where({ userId })
-    : await db.from("notes");
+    ? await db("notes").where({ userId })
+    : await db("notes");
   return notes;
 }
 
 async function addNote(newNote) {
-  const [newNoteId] = await db.from("notes").insert(newNote);
-  const [noteAdded] = await db.from("notes").where({ id: newNoteId });
+  const [newNoteId] = await db("notes").insert(newNote);
+  const noteAdded = await db("notes").where({ id: newNoteId }).first();
   return noteAdded;
 }
 
